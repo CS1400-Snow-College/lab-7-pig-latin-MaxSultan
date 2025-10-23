bd0bc44392d8d75afbe7c9b3e056e3428c3b5daa
Max Sultan <maxsultankent@gmail.com>
2025-10-22 22:36:36 -0600

    Offset each char; print offset

--- Program.cs
+++ Program.cs
@@ -11,7 +11,29 @@ string TraslateToPigLatin (string text){
     }));
 } 
 
+int UpperBounded (int min, int max, int number) {
+    while (number > max) {
+        int overflow = number - max;
+        number = min + overflow;
+    }
+    return number;
+}
+
+string OffsetCharacter(string text, int seed){
+    int characterMin = 96;
+    int characterMax = 122;
+    return string.Join(" ", text.Split(" ").Select(word =>
+        string.Join("", word.ToCharArray().Select(letter => {
+            Console.WriteLine($"{UpperBounded(characterMin, characterMax, (int)letter + seed)} {(char)UpperBounded(characterMin, characterMax, (int)letter + seed)}");
+            return (char)(UpperBounded(characterMin, characterMax, (int)letter + seed));
+        }))
+    ));
+}
+
+Random rand = new Random();
 Console.WriteLine("Welcome to Pig Latin encrytion tool");
 Console.Write("Please enter the message: ");
 string input = Console.ReadLine();
 Console.WriteLine($"In pig latin that's: {TraslateToPigLatin(input)}");
+int seed = rand.Next(1,26);
+Console.WriteLine($"We can encrypt that as: {OffsetCharacter(TraslateToPigLatin(input), seed)}");
--- obj/Debug/net8.0/lab-7-pig-latin-MaxSultan.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-7-pig-latin-MaxSultan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-7-pig-latin-MaxSultan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+921b9227e32622a726c308ad218a63233cbc2fb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7912226d6ef5ff684c3ceb638eec18ae3c80c735")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-7-pig-latin-MaxSultan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-7-pig-latin-MaxSultan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
