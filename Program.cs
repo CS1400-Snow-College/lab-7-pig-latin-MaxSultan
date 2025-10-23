02a0dfd23ee117de7501dacc3fe65d65433decdd
Max Sultan <maxsultankent@gmail.com>
2025-10-22 20:53:00 -0600

    greeting

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
 ﻿// Max Sultan, October 22nd, Lab 7: Pig Latin
- 
+
+Console.WriteLine("Welcome to Pig Latin encrytion tool")
+// If your word starts with a consonant, or a consonant cluster, move the cluster to the end and add AY, so Frank becomes ankfray. If your word begins with a vowel and ends with a vowel just add 'way', so apple becomes appleway
